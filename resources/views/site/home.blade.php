ec98e84235dc287723946c4530a24ee64cf30af6
odenilson marques <odmarquesgm@gmail.com>
2023-12-14 09:00:55 -0300

    insert plans in page home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -13,9 +13,8 @@
                     para materializar visões e deixar uma marca única no mundo.
                 </p>
 
-                <button class="btn btn-primary">Começe agora mesmo 
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                <button class="btn btn-primary">Começe agora mesmo
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-arrow-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
@@ -32,17 +31,54 @@
     </section>
 
     <section class="container plans-section">
-        <div class="row">
-            <h1>Inserir aqui o texto</h1>
-            <div class="col-lg-6 col-md-6 col-sm-12 hero-text">
-                <!-- Conteúdo de texto aqui -->
-                <h1></h1>
-                <p>
-                    Construa o amanhã com suas ideias. Explore possibilidades, seja protagonista na criação. Junte-se a nós
-                    para materializar visões e deixar uma marca única no mundo.
-                </p>
-
-                
+        <div class="row justify-content-center">
+            <h1>Plano que melhor se adapta a você</h1>
+            <p>Escolha o plano que melhor se encaixa as suas necessidades</p>
+            <div class="col-4">
+                <div class="card bg-light mb-3" style="max-width: 30rem;">
+                    <div class="card-header">Mensal</div>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">R$ 19,90/mês</h5>
+                        <ul class=" list-checked list-checked-primary">
+                            <li class=" list-checked-item">teste</li>
+                            <li>teste</li>
+                            <li>teste</li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <a href=""><button class="btn btn-primary">Assinar agora mesmo
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                            </svg>
+                        </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card text-dark bg-light mb-3" style="max-width: 30rem;">
+                    <div class="card-header">Anual</div>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">R$ 39,90/mês</h5>
+                        <ul class=" list-checked list-checked-primary">
+                            <li class=" list-checked-item">teste</li>
+                            <li>teste</li>
+                            <li>teste</li>
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        <a href=""><button class="btn btn-primary">Assinar agora mesmo
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                            </svg>
+                        </button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </section>

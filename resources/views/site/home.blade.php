1b39ba4458cfbb997aad6f96841e6bdbd4c9bb81
odenilson marques <odmarquesgm@gmail.com>
2023-12-15 16:54:08 -0300

    create form of contact

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -29,37 +29,69 @@
 
     <section class="container plans-section">
         <div class="row justify-content-center">
-            <h1>Plano que melhor se adapta a você</h1>
-            <p>Escolha o plano que melhor se encaixa as suas necessidades</p>
+            <div class="col-md-12 text-center mb-4">
+                <h1>Plano que melhor se adapta a você</h1>
+                <p>Escolha o plano que melhor se encaixa às suas necessidades</p>
+            </div>
+    
             @foreach ($plans as $plan)
-                <div class="col-4">
-                    <div class="card bg-light mb-3" style="max-width: 30rem;">
+                <div class="col-md-4 mb-4">
+                    <div class="card bg-light">
                         <div class="card-header">{{ $plan->name }}
                             @if ($plan->recomended)
-                                <span class="badge">Recomendado</span>
+                                <span class="badge badge-primary">Recomendado</span>
                             @endif
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title text-center"><span class="display-cifrao">R$</span>{{number_format($plan->price, 2, ',', '.')}}</span></h5>
-                            <ul class=" list-checked list-checked-primary">
+                            <h5 class="card-title text-center"><span class="display-cifrao">R$</span>{{ number_format($plan->price, 2, ',', '.') }}</span></h5>
+                            <ul class="list-checked list-checked-primary">
                                 @foreach ($plan->features as $feature)
-                                <li>{{$feature->name}}</li>
+                                    <li>{{ $feature->name }}</li>
                                 @endforeach
                             </ul>
                         </div>
-                        <div class="card-footer">
-                            <a href="{{route('assina.plan',$plan->url)}}"><button class="btn btn-primary">Assinar agora mesmo
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                        fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                                    </svg>
-                                </button>
+                        <div class="card-footer text-center">
+                            <a href="{{ route('assina.plan', $plan->url) }}" class="btn btn-primary">Assinar agora
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg>
                             </a>
                         </div>
                     </div>
                 </div>
             @endforeach
+        </div>
+    </section>
+    
+
+
+    <section class="container-fluid form-section">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="shadow p-3 mb-5 bg-body rounded">
+                    <form action="" method="">
+                        @csrf
+                        <h2 class="text-center mb-4">Fale conosco</h2>
+                        <div class="form-group mb-3">
+                            <label for="nome">Nome</label>
+                            <input type="text" class="form-control" id="nome" name="nome" required>
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="email">E-mail</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="mensage">Mensagem</label>
+                            <textarea class="form-control"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+                    </form>
+                </div>
+
+            </div>
         </div>
     </section>
 @endSection
